d1a59c943d1a1c119963e5e97c96b28b00e90a4e
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-08 10:05:15 -0600

    Feat: Add Redux Slice.

---
+++ app/javascript/slices/greetingSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// Async thunk action
+export const fetchGreeting = createAsyncThunk(
+  'greetings/fetchGreeting',
+  async () => {
+    const response = await axios.get('/api/v1/greetings');
+    return response.data.greeting;
+  }
+);
+
+const greetingSlice = createSlice({
+  name: 'greeting',
+  initialState: { entity: '', status: 'idle', error: null },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchGreeting.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchGreeting.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.entity = action.payload;
+      })
+      .addCase(fetchGreeting.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default greetingSlice.reducer;
