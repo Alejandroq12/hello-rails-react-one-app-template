6b5ffb29a7e2cfad0ba02f49d2703985b54b3738
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-08 10:06:01 -0600

    Fix: Import route in application.js.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,7 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App.js';
 import { Provider } from 'react-redux';
-import store from './store.js';
+import store from './redux/store.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
